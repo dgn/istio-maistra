gangmuk/istio

pilot: skip privileged ports when building listeners for non-root gateways (#28268)
* pilot: skip privileged ports when building listeners for non-root gateways

* Add release note

* Use ISTIO_META_UNPRIVILEGED_POD env var instead of a Pod label

pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -1406,6 +1406,40 @@ func TestBuildGatewayListeners(t *testing.T) {
 			},
 			[]string{"0.0.0.0_80", "0.0.0.0_801"},
 		},
+		{
+			"privileged port on unprivileged pod",
+			&pilot_model.Proxy{
+				Metadata: &pilot_model.NodeMetadata{
+					UnprivilegedPod: "true",
+				},
+			},
+			&networking.Gateway{
+				Servers: []*networking.Server{
+					{
+						Port: &networking.Port{Name: "http", Number: 80, Protocol: "HTTP"},
+					},
+					{
+						Port: &networking.Port{Name: "http", Number: 8080, Protocol: "HTTP"},
+					},
+				},
+			},
+			[]string{"0.0.0.0_8080"},
+		},
+		{
+			"privileged port on privileged pod",
+			&pilot_model.Proxy{},
+			&networking.Gateway{
+				Servers: []*networking.Server{
+					{
+						Port: &networking.Port{Name: "http", Number: 80, Protocol: "HTTP"},
+					},
+					{
+						Port: &networking.Port{Name: "http", Number: 8080, Protocol: "HTTP"},
+					},
+				},
+			},
+			[]string{"0.0.0.0_80", "0.0.0.0_8080"},
+		},
 	}
 
 	for _, tt := range cases {
@@ -1414,6 +1448,12 @@ func TestBuildGatewayListeners(t *testing.T) {
 		})
 		proxy := cg.SetupProxy(&proxyGateway)
 		proxy.ServiceInstances = tt.node.ServiceInstances
+		if tt.node.Metadata != nil {
+			proxy.Metadata = tt.node.Metadata
+		} else {
+			proxy.Metadata = &proxyGatewayMetadata
+		}
+
 		builder := cg.ConfigGen.buildGatewayListeners(&ListenerBuilder{node: proxy, push: cg.PushContext()})
 		listeners := xdstest.ExtractListenerNames(builder.gatewayListeners)
 		sort.Strings(listeners)

dgn/istio-maistra

pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -1244,6 +1244,40 @@ func TestBuildGatewayListeners(t *testing.T) {
 			},
 			[]string{"0.0.0.0_80", "0.0.0.0_801"},
 		},
+		{
+			"privileged port on unprivileged pod",
+			&pilot_model.Proxy{
+				Metadata: &pilot_model.NodeMetadata{
+					UnprivilegedPod: "true",
+				},
+			},
+			&networking.Gateway{
+				Servers: []*networking.Server{
+					{
+						Port: &networking.Port{Name: "http", Number: 80, Protocol: "HTTP"},
+					},
+					{
+						Port: &networking.Port{Name: "http", Number: 8080, Protocol: "HTTP"},
+					},
+				},
+			},
+			[]string{"0.0.0.0_8080"},
+		},
+		{
+			"privileged port on privileged pod",
+			&pilot_model.Proxy{},
+			&networking.Gateway{
+				Servers: []*networking.Server{
+					{
+						Port: &networking.Port{Name: "http", Number: 80, Protocol: "HTTP"},
+					},
+					{
+						Port: &networking.Port{Name: "http", Number: 8080, Protocol: "HTTP"},
+					},
+				},
+			},
+			[]string{"0.0.0.0_80", "0.0.0.0_8080"},
+		},
 	}
 
 	for _, tt := range cases {
@@ -1252,6 +1286,11 @@ func TestBuildGatewayListeners(t *testing.T) {
 		env := buildEnv(t, []pilot_model.Config{{Spec: tt.gateway}}, []pilot_model.Config{})
 		proxyGateway.SetGatewaysForProxy(env.PushContext)
 		proxyGateway.ServiceInstances = tt.node.ServiceInstances
+		if tt.node.Metadata != nil {
+			proxyGateway.Metadata = tt.node.Metadata
+		} else {
+			proxyGateway.Metadata = &proxyGatewayMetadata
+		}
 		proxyGateway.DiscoverIPVersions()
 		builder := configgen.buildGatewayListeners(&proxyGateway, env.PushContext, &ListenerBuilder{})
 		var listeners []string